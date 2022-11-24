1a37a3a15c3c4173116fa31a42a8bbb6ef9e177b
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-11-24 08:48:19 +0300

    1. Загрузка данных в вид [#295782 #363639]

--- src/main/java/ru/job4j/accidents/controller/IndexControl.java
+++ src/main/java/ru/job4j/accidents/controller/IndexControl.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
 
 @Controller
+@AllArgsConstructor
 public class IndexControl {
 
     private AccidentService accidentService;
--- src/main/java/ru/job4j/accidents/service/AccidentService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.repository.AccidentMem;
@@ -7,6 +8,7 @@ import ru.job4j.accidents.repository.AccidentMem;
 import java.util.HashMap;
 
 @Service
+@AllArgsConstructor
 public class AccidentService {
 
     private AccidentMem accidentMem;
