c5b8b8dca3759b8ecc94e4f723096c2e7a5ba35b
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-11-28 15:02:24 +0300

    2. Регистрация пользователя [#296069]

---
+++ src/main/java/ru/job4j/accidents/model/User.java
@@ -0,0 +1,29 @@
+package ru.job4j.accidents.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String password;
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+
+    private boolean enabled;
+}
